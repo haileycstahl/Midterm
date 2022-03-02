e954568aa36f8b6e45f4d5b43554f64d659de90f
Hailey Stahl <hailey.stahl@gmail.com>
2022-03-02 12:36:10 -0600

    UPDATED

--- MidtermRCode.R
+++ MidtermRCode.R
@@ -3,22 +3,51 @@
 ### Use the follwoing three predictors Age_08_04, KM, Fuel_Type
 library(forecast)
 
-car.df <- read.csv("ToyotaCorolla.csv")
+data <- read.csv("ToyotaCorolla.csv")
+
+#1 is petrol, 0 diseal
+data$Fuel_Type_cat<-ifelse(data$Fuel_Type =="Petrol",1,0)
+
+datasub<-subset(data,select = c("Age_08_04","KM","Fuel_Type_cat","Price"))
+View(datasub)
 
 # partition data into Training (70%) and Validation (30%) datasets
+set.seed(1)
+train_row <- sample(rownames(datasub), dim(datasub)[1]*0.70)
+train_data <- datasub[train_row, ]
+
+valid_rows <- setdiff(rownames(datasub), train_row) 
+valid_data <- datasub[valid_rows, ]
+
+View(valid_data)
+View(train_data)
+
 
 # use lm() to run a linear regression of Price on the 
-# three predictors in the training set. 
 
+reg<-lm(Price~data$Age_08_04+KM+Fuel_Type_cat,data=datasub,subset =train_row)
+
+tr.res <- data.frame(train_data$Price, reg$fitted.values, reg$residuals)
+print(head(tr.res))
 
 #  use options() to ensure numbers are not displayed in scientific notation.
 options(scipen = 999)
 
 # Display the results (summary()) of the linear regression
+options(scipen = 999)
+print(summary(reg))
 
 # use predict() to make predictions on a new set. 
+pred <- predict(reg, data= valid_data)
+
 
 # use accuracy() to compute common accuracy measures.
-# recall that accuracy is computed against the validation dataset
+options(scipen = 999)
+accuracy(pred, valid_data$Price)
+
+
+#accuracy of training
+options(scipen = 999)
+accuracy(reg$fitted.values, train_data$Price)
 
 
